f4436f11b514e98a5a23782f7fd835d735815ace
Marco Rezon <marcorezonmoura1@gmail.com>
2023-11-27 18:58:00 -0400

    first day commit

---
+++ JS.js
@@ -0,0 +1,11 @@
+document.getElementById("to-introduction-button").onclick = function () {
+    location.href = "introduction/introduction.html";
+};
+
+document.getElementById("back-to-welcome-button").onclick = function () {
+    location.href = "index.html";
+};
+
+document.getElementById("to-form-1-button").onclick = function () {
+    location.href = "introduction/form/form1.html";
+};
