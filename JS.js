2f642ca87822807fe7d5a621674a84fcf1e630f5
Marco Rezon <marcorezonmoura1@gmail.com>
2023-11-28 22:02:38 -0400

    second day commit

--- JS.js
+++ JS.js
@@ -1,11 +1,22 @@
 document.getElementById("to-introduction-button").onclick = function () {
-    location.href = "introduction/introduction.html";
+    window.location.href = "introduction/introduction.html";
 };
 
 document.getElementById("back-to-welcome-button").onclick = function () {
-    location.href = "index.html";
+    window.location.href = "https://stackoverflow.com/questions/15759020/window-location-href-doesnt-redirect";
+    alert('worked')
 };
 
 document.getElementById("to-form-1-button").onclick = function () {
-    location.href = "introduction/form/form1.html";
+    window.location.href = "introduction/form/form1.html";
 };
+
+
+
+function switchContent (passedId, newText) {
+    document.getElementById(passedId).innerHTML = newText;
+}
+
+function getContentBack (passedId, originalText) {
+    document.getElementById(passedId).innerHTML = originalText;
+}
